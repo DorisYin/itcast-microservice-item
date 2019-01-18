010913a2e3c40f3fda60907d018fc7a8a07cbd4c
DorisYin <543936753@qq.com>
2019-01-18 23:57:26 +0800

    first

---
+++ src/main/java/cn/itcast/microservice/pojo/Item.java
@@ -0,0 +1,12 @@
+package cn.itcast.microservice.pojo;
+
+/**
+ * Created by DorisYin on 2019/1/18.
+ */
+public class Item {
+    private long id;
+    private String title;
+    private String pic;
+    private String desc;
+    private Long price;
+}
---
+++ src/main/java/cn/itcast/microservice/service/ItemService.java
@@ -0,0 +1,7 @@
+package cn.itcast.microservice.service;
+
+/**
+ * Created by DorisYin on 2019/1/18.
+ */
+public class ItemService {
+}
